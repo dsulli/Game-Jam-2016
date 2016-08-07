8d4e81b0a6bd513b0fd3d34c2b962da1d006d698
Ben Young <benstrife@gmail.com>
2016-08-06 22:41:12 -0700

    I DID PROGAMING

---
+++ BANDKIDS/Assets/Scripts/OffsetScroller.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OffsetScroller : MonoBehaviour {
+
+	public float scrollSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		float x = Mathf.Repeat (Time.time * scrollSpeed, 1);
+		Vector2 offset = new Vector2 (-x, 0);
+		GetComponent<Renderer>().sharedMaterial.SetTextureOffset ("_MainTex", offset);
+		//renderer.sharedMaterial.SetTextureOffset ("_MainTex", offset);
+
+	}
+}
