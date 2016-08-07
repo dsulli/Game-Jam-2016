f3458c94741a6bc56759b42d2ccd99b65268df23
Ben Young <benstrife@gmail.com>
2016-08-06 23:03:05 -0700

    Added Scrolling Big Bkgd

--- BANDKIDS/Assets/Scripts/OffsetScroller.cs
+++ BANDKIDS/Assets/Scripts/OffsetScroller.cs
@@ -4,19 +4,22 @@ using System.Collections;
 public class OffsetScroller : MonoBehaviour {
 
 	public float scrollSpeed;
+	private Vector2 savedOffset;
 
 	// Use this for initialization
 	void Start () {
-	
+		savedOffset = GetComponent<Renderer> ().sharedMaterial.GetTextureOffset ("_MainTex");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 		float x = Mathf.Repeat (Time.time * scrollSpeed, 1);
-		Vector2 offset = new Vector2 (-x, 0);
+		Vector2 offset = new Vector2 (-x,savedOffset.y);
 		GetComponent<Renderer>().sharedMaterial.SetTextureOffset ("_MainTex", offset);
-		//renderer.sharedMaterial.SetTextureOffset ("_MainTex", offset);
+	}
 
+	void OnDisable () {
+		GetComponent<Renderer> ().sharedMaterial.SetTextureOffset ("_MainTex", savedOffset);
 	}
 }
